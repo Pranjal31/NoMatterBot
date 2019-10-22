82d09afe5d7aa8bbf00c4bff1c469dea7dfcb2cb
Vaishakh <v.bhadrappanavar@f5.com>
2019-10-22 18:20:34 -0400

    Stale Issues Implementation

--- code/stale.js
+++ code/stale.js
@@ -70,11 +70,11 @@ function getDefaultOptions(endpoint, method)
 }
 
 function sixMonthsPrior(date) {
-    //Get current data
+    //Today's date
     var d = new Date(date);
-    // Get month
+    // This Month
     var m = d.getMonth();
-    // Subtract 6 months
+    // 6 months Prior
     d.setMonth(d.getMonth() - 6);
     // If the new month number isn't m - 6, set to last day of previous month
     // Allow for cases where m < 6
@@ -87,123 +87,46 @@ function sixMonthsPrior(date) {
 
 async function Stale_Issues()
 {
-    //let options = getDefaultOptions("/issues", "GET");
 
-    var list = [];                                        // To capture list of Stale Issues
+    var list = [];                     // To capture list of Stale Issues
+    present = Date();                 //Todays date
+    old = sixMonthsPrior(present);                                   
+            
+
+    var map1 = new Map();
+    var str = "";          
+  
+    var obj = getIssues();                          //Get Issues currently reading from mockIssues.json
+		
+	for( var i = 0; i < obj.length; i++ )
+	{
+        var title = obj[i].title;
+        var updated = obj[i].updated_at;
+        var state =  obj[i].state;
+
+        lm = Date.parse(updated);             //last_modified date of Git hub issue
+               
+        th=old;                              //Threshold /6 months/ date set
+               
+            
+        if(th>lm)                               //compare threshold and last modified
+        {
+            list.push(obj[i].title);
+            map1.set(obj[i].title,obj[i].number);
+            str += obj[i].title + "\t: " + obj[i].number + '\n'
+        }
+    }
+ 
+        var channel_id = await library.createChannel();
     
-	// Send a http request to url and specify a callback that will be called upon its return.
-	//return new Promise(function(resolve, reject)
-	//{
-	//	request(options, async function (error, response, body) 
-	//	{
-	//		if( error )
-	//		{
-	//			console.log( chalk.red( error ));
-	//			reject(error);
-	//			return; // Terminate execution.
-	//		}
-
-
-            present = Date();                 //Todays date
-
-            old = sixMonthsPrior(present);                                   //Use this function to calculate 6 months old date from present date
-            //old = Date.parse('2019-10-20T16:51:17.000Z');                  //Put the old date here beyond which we need to calculate stale issues
-            //console.log(present);                                         //Log present date
-            //console.log(old);                                              //log old date
-            //console.log(typeof(sixMonthsPrior(present)));
-            //console.log(formatDate(sixMonthsPrior(present)));
-
-            var map1 = new Map();
-            var str = "";          
-
-
-
-            //console.log(user)
-            
-            //var map1 = new Map(["Issue Number","Title"]);
-
-        //	var obj = JSON.parse(body);
-            var obj = getIssues();                          //Get Issues currently reading from mockIssues.json
-		//	console.log("List of Repositories for user are as follows:")
-			for( var i = 0; i < obj.length; i++ )
-			{
-                var title = obj[i].title;
-                var updated = obj[i].updated_at;
-                var state =  obj[i].state;
-
-                lm = Date.parse(updated);             //last_modified date of Git hub issue
-               // th = Date.parse(old);
-                th=old;                              //Threshold /6 months/ date set
-                //console.log("last modified:");
-                //console.log(lm);
-
-                //console.log("Threshold 6 months date");
-                //console.log(th);
-            
-                if(th>lm)                               //compare threshold and last modified
-                {
-                //    console.log("yup");
-                    //console.log(title);
-                    
-                    list.push(obj[i].title);
-                    map1.set(obj[i].title,obj[i].number);
-                    //console.log(obj[i].title);
-                    //console.log(obj[i].title);
-                   // str.concat(obj[i].title)
-                    //str.concat(":")
-                    //str.concat(obj[i].number)
-                    //str.concat("\n")
-                    //console.log(typeof(obj[i].title));
-                    str += obj[i].title + "\t: " + obj[i].number + '\n'
-                }
-            }
-            //console.log(str);
-            //console.log("Complete list of stale issues")
-            //console.log(list);
-            var map;
-
-            object = {};
-            object1 = {};
-
-            var fin=map1;
-
-            map1.forEach((value, key) => {
-                var keys = key.split('.'),
-                    last = keys.pop();
-                keys.reduce((r, a) => r[a] = r[a] || {}, object)[last] = value;
-            });
-
-            var info = "";
-
-
-            object1 = JSON.stringify(object);
-            //console.log(object1);
-
-           
-           map2 = JSON.stringify(map1);
-           //console.log(map1);
-           map = JSON.stringify(list);
-
-            var channel_id = await library.createChannel();
-
-            
-        
-            
-
-            //console.log(user);
-            
-            //console.log(channel_id);
-            
-            var payload = {
+        var payload = {
                 "channel_id": channel_id,
-                 "message": "Hey, Bot's up? \n The following open issues have had no activity in the last 6 months.",
-                 //"elements": "list", 
-                 //"body":"map",
+                 "message": "Hey, Bot's up? \n The following open issues have had no activity in the last 6 months.", 
                  "props": {
                      "attachments": [
                          {
                              "pretext": "Do you want to close them?",
-                             "text": str,   //str
+                             "text": str,   
                              
                              "actions": [
                                  {
@@ -236,20 +159,7 @@ async function Stale_Issues()
             
             return respBody.id;
             
-            
-
-			// Return object for people calling our method.
-			//resolve( obj );
-//		});
-//	});
-
 }
-
-
-
-
-
-
 
 module.exports.Stale_Issues = Stale_Issues;
 module.exports.getIssues = getIssues;
