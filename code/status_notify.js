7293f931306eff16716da14b6fd5447914fa6d6a
Sridhar <smkulka3@ncsu.edu>
2019-10-21 21:05:50 -0400

    removed old code

--- code/status_notify.js
+++ code/status_notify.js
@@ -1,20 +1,12 @@
 var lib = require('./lib');
-//var server = require('./server/index.js')
-
-var config = {};
-
-// Retrieve our api token from the environment variables.
-//config.channel = process.env.CHANNELID;
 
 var data = {}
-//data.channel_id = config.channel;
 
 async function notify_change(post_body)
 {
-    console.log("in notify");
     var msg = "";
 
-    var chId = lib.createChannel("abcd");
+    var chId = await lib.createChannel("abcd");
     data.channel_id = chId;
 
     if (post_body.action === 'opened')
