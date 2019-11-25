22b548e96573e48152feadc952f15d0085eade15
Pranjal Sharma <psharma9@ncsu.edu>
2019-11-25 18:50:36 -0500

    added robustness against conflicting status labels

--- code/statuschange.js
+++ code/statuschange.js
@@ -1,42 +1,28 @@
-const chalk  = require('chalk');
 var lib = require('./lib');
 
-if( !lib.config.gh_token )
-{
-	console.log(chalk`{red.bold GITHUBTOKEN is not defined!}`);
-	console.log(`Please set your environment variables with appropriate token.`);
-	console.log(chalk`{italic You may need to refresh your shell in order for your changes to take place.}`);
-	process.exit(1);
+// Update status label on issue
+function isStatusLabel(labelName) {
+	var label_ir = "in review";
+	var label_test = "in test";
+	var label_ip = "in progress"
+	return ( labelName === label_ir || labelName === label_ip || labelName === label_test )
 }
 
 // Update status label on issue
 async function updateStatusLabelOnIssue(owner, repo, issue, label) {
-	var label_ir = "in review";
-	var label_test = "in test";
-	var label_ip = "in progress"
-	var hasStatus = false
-	var labelName = ""
-	
 	issueLabels = await lib.getLabelsOnIssue(owner,repo, issue);
-	if (issueLabels.length > 0) {
-		for ( var labelIdx in issueLabels ) {	
-			// no need to update label
-			if ( issueLabels[labelIdx].name === label ) {
-				return;
-			} else if ( issueLabels[labelIdx].name === label_ir || issueLabels[labelIdx].name === label_ip || issueLabels[labelIdx].name === label_test ) {
-				hasStatus = true
-				labelName = issueLabels[labelIdx].name
-				break;
-			} 
+	if ( issueLabels.length > 0 ) {
+		var labelName = ""
+		// delete any existing status labels
+		for ( var labelIdx in issueLabels ) {
+			labelName = issueLabels[labelIdx].name.toLowerCase()
+			if ( isStatusLabel(labelName) ) {
+				await lib.deleteLabelOnIssue(owner, repo, issue, labelName);
+			}
 		}
 	} 
-	// delete status label if present
-	if (hasStatus) {
-		await lib.deleteLabelOnIssue(owner, repo, issue, labelName);
-	}
+	// add desired status label
 	await lib.addLabelOnIssue(owner, repo, issue, label);
 }
 
 module.exports.updateStatusLabelOnIssue = updateStatusLabelOnIssue;
-
-
