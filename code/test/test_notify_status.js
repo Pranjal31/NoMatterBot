bd39e97f804a5c47e41a0dab9bb9bfc31678e5ca
Pranjal Sharma <psharma9@ncsu.edu>
2019-11-30 13:21:26 -0500

    notify test passing

--- code/test/test_notify_status.js
+++ code/test/test_notify_status.js
@@ -10,8 +10,6 @@ var config = {};
 
 // retrieve api tokens
 config.mmurl = process.env.MATTERMOSTURL;
-//config.userchannel = process.env.CHANNELUSERID;	//TODO
-//config.botchannel = process.env.BOTUSERID;		//TODO
 config.loginEmail = process.env.MATTERMOST_EMAIL;	
 config.channelname = process.env.CHANNELNAME;
 config.loginPassword = process.env.MATTERMOST_PWD;
@@ -25,7 +23,7 @@ describe('Notify status using puppeteer', function () {
   this.timeout(5000000);
 
   beforeEach(async () => {
-      browser = await puppeteer.launch({headless: true, args: ["--no-sandbox", "--disable-web-security"]}); //TODO
+      browser = await puppeteer.launch({headless: true, args: ["--no-sandbox", "--disable-web-security"]}); 
       page = await browser.newPage();
       await page.goto(`${config.mmurl}/login`, {waitUntil: 'networkidle0'});
       await page.type('input[id=loginId]', config.loginEmail);
@@ -42,13 +40,11 @@ describe('Notify status using puppeteer', function () {
 
   it('Should show status notification', async () => {
 
-    var expectedMsg = "Issue: #24 ahahah in repo: test1 is now in progress";	//TODO
+    var expectedMsg = "Issue: #24 ahahah in repo: test1 is now in progress";	
 
-//    var channelName = config.userchannel+"__"+config.botchannel;	// TODO
-    var channelName = config.channelname;		// TODO
-//    msgId = await notifier.notify_change(mockStatChange);	//TODO
+    var channelName = config.channelname;		
 
-    msgId = notifier.notifyStatChange("psharma9","test1","24","ahahah", "in progress")	//FIXME //TODO
+    msgId = await notifier.notifyStatChange("psharma9", mockStatChange.repository.name, mockStatChange.issue.number, mockStatChange.issue.title, mockStatChange.label.name);
 
     var postId = "postMessageText_" + msgId;
 
