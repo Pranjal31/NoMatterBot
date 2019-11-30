970935536f75cca86ab153956f0116d500a4a71e
Pranjal Sharma <psharma9@ncsu.edu>
2019-11-30 14:20:39 -0500

    stale issues it passing

--- code/test/test_stale.js
+++ code/test/test_stale.js
@@ -1,7 +1,5 @@
 const puppeteer = require('puppeteer');
-//const assignee_recommend = require('../assignee_recommend.js');
 const lib = require('../lib.js');
-// const mockStatChange = require('../mock_statChange.json');	//FIXME //TODO
 const chalk = require('chalk');
 const stale = require('../stale.js');
 
@@ -12,7 +10,7 @@ const nock = require("nock");
 const mockIssues = require('../mockIssues.json');
 const mockUser = require('../mockUser.json');	
 const mockRepos = require('../mockRepos.json');		
-const mockAssignee = "psharma9";	//TODO
+const mockAssignee = "psharma9";	
 
 var config = {};
 
@@ -27,7 +25,7 @@ describe('Close or Ignore stale Issues', function () {
   // MOCK SERVICE
   var mockIssueService = nock("https://api.github.ncsu.edu")
     .persist() // This will persist mock interception for lifetime of program.
-    .get("/repos/psharma9/test1/issues")	//TODO
+    .get("/repos/psharma9/test1/issues")	
     .reply(200, JSON.stringify(mockIssues) );
 
   var mockUserService = nock("https://api.github.ncsu.edu")
@@ -40,20 +38,6 @@ describe('Close or Ignore stale Issues', function () {
     .get("/user/repos?affiliation=owner")
     .reply(200, JSON.stringify(mockRepos) );
 
-
-//TODO
-/*  var mockUserService = nock("https://api.github.ncsu.edu")
-    .persist() // This will persist mock interception for lifetime of program.
-    .get("/repos/psharma9/test1/collaborators")		//TODO
-    .reply(200, JSON.stringify(mockUsers) );
-*/
-
-// TODO
-/*  var mockAssignService = nock("https://api.github.ncsu.edu")
-    .persist() // This will persist mock interception for lifetime of program.
-    .patch("/repos/psharma9/test1/issues/" + mockNewIssue.issue_id)	
-    .reply(200, JSON.stringify(mockUsers) ); */
-
   let browser;
   let page;
   let msgId;
@@ -61,7 +45,7 @@ describe('Close or Ignore stale Issues', function () {
   this.timeout(5000000);
 
   beforeEach(async () => {
-      browser = await puppeteer.launch({headless: true, args: ["--no-sandbox", "--disable-web-security"]});	//TODO
+      browser = await puppeteer.launch({headless: true, args: ["--no-sandbox", "--disable-web-security"]});	
       page = await browser.newPage();
       await page.goto(`${config.mmurl}/login`, {waitUntil: 'networkidle0'});
       await page.type('input[id=loginId]', config.loginEmail);
@@ -97,39 +81,7 @@ describe('Close or Ignore stale Issues', function () {
 
     await browser.close();
   });
-//TODO  -- as of now stale issue code isn't responsible for such a message
-/*
-  it('Should close Stale Issues', async () => {
 
-    var expectedMsg2 = "Cool. It's done!";
-
-    var postId = "messageAttachmentList_" + msgId;
-
-    await page.waitForSelector('#sidebarItem_'+ config.channelName);
-    await page.click('#sidebarItem_'+ config.channelName);
-
-    let selector = `#${postId} > div > div.attachment__content > div > div > div.attachment__body.attachment__body--no_thumb > div.attachment-actions > button:nth-child(1)`;
-
-    await page.waitForSelector(selector);
-
-    await page.click(selector);
-
-    // await page.evaluate((selector) => document.querySelector(selector).click(), selector);
-
-    var close_msgId = await stale.close_all();
-
-    var close_post = "postMessageText_" + close_msgId;
-
-    //console.log(close_post);
-
-    const textEle = await page.$x(`//*[contains(@id, "${close_post}")]/p`);
-    const text = await (await textEle[0].getProperty('textContent')).jsonValue();
-
-    expect(text).to.eql(expectedMsg2);
-
-    await browser.close();
-  });
-*/
   it('Should ignore Stale Issues', async () => {
 
     var expectedMsg3 = "Alright! These issues(s) have been ignored for a day.";
@@ -145,13 +97,9 @@ describe('Close or Ignore stale Issues', function () {
 
     await page.click(selector);
 
-    // await page.evaluate((selector) => document.querySelector(selector).click(), selector);
-
-    var ignore_msgId = await stale.ignoreAll(mockAssignee);	//TODO
+    var ignore_msgId = await stale.ignoreAll(mockAssignee);	
 
     var ignore_post = "postMessageText_" + ignore_msgId;
-
-    //console.log(ignore_post);
 
     const textEle = await page.$x(`//*[contains(@id, "${ignore_post}")]/p`);
     const text = await (await textEle[0].getProperty('textContent')).jsonValue();
